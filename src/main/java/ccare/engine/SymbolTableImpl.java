81d5a7245a23c298e5d595fc53ec8d4320ec5d53
Charles Care <c.p.care@gmail.com>
2010-10-31 12:31:15 +0000

    Pulled service and symboltable apart

---
+++ src/main/java/ccare/engine/SymbolTableImpl.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright (c) 2010, Charles Care
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * * Neither the name of the author nor the names of its contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package ccare.engine;
+
+import ccare.domain.*;
+import ccare.domain.Observable;
+import ccare.service.SymbolTableService;
+
+import javax.ejb.Lock;
+import javax.ejb.Singleton;
+import java.util.*;
+
+import static javax.ejb.LockType.READ;
+import static javax.ejb.LockType.WRITE;
+
+@Singleton
+@Lock(READ)
+public class SymbolTableImpl implements SymbolTableService, SymbolTable {
+
+    private final UUID id = UUID.randomUUID();
+    private Map<SymbolReference, Object> values = new HashMap<SymbolReference, Object>();
+    private Map<SymbolReference, String> defns = new HashMap<SymbolReference, String>();
+    private Map<SymbolReference, Symbol> symbols = new HashMap<SymbolReference, Symbol>();
+
+    @Override
+    public UUID getId() {
+        return id;
+    }
+
+    @Override
+    @Lock(WRITE)
+    public void define(SymbolReference reference, Observable d) {
+        defns.put(reference, d.getDefinition());
+        values.put(reference, d.getCurrentValue());
+    }
+
+    @Override
+    public void add(Symbol sym) {
+        symbols.put(sym.getReference(), sym);
+    }
+
+    @Override
+    public Observable observe(SymbolReference reference) {
+        Observable o = new Observable();
+        o.setDefinition(defns.get(reference));
+        o.setCurrentValue(values.get(reference));
+        return o;
+    }
+
+    @Override
+    public Symbol get(SymbolReference reference) {
+        if (!symbols.containsKey(reference)) {
+            add(new SymbolImpl(reference));
+        }
+        return symbols.get(reference);
+    }
+
+    @Override
+    public Set<SymbolReference> listSymbols() {
+        return Collections.unmodifiableSet(values.keySet());
+    }
+
+    @Override
+    public void fireTriggers(Set<Symbol> triggers) {
+        for (Symbol s : triggers) {
+            this.execute(s.getReference());
+        }
+    }
+
+    @Override
+    public void define(SymbolReference ref, String defn) {
+        final JavaScriptDefinition d = new JavaScriptDefinition(defn);
+        Symbol s = get(ref);
+        s.redefine(d, this);
+    }
+
+    @Override
+    public Object getValue(SymbolReference ref) {
+        Symbol s = get(ref);
+        return s.getValue(this);
+    }
+
+    @Override
+    public void defineFunction(SymbolReference ref, String defn) {
+        final JavaScriptDefinition d = new JavaScriptDefinition(defn, JavaScriptDefinition.ExprType.FUNCTION);
+        Symbol s = get(ref);
+        s.redefine(d, this);
+    }
+
+    @Override
+    public void defineTriggeredProc(SymbolReference ref, String defn, String... triggers) {
+        final JavaScriptDefinition d = new JavaScriptDefinition(defn, JavaScriptDefinition.ExprType.FUNCTION, triggers);
+        Symbol s = get(ref);
+        s.redefine(d, this);
+    }
+
+    @Override
+    public void execute(SymbolReference a) {
+        final SymbolReference r = new SymbolReference("____DUMMY_REF");
+        define(r, "#{" + a.getName() + "}()");
+        getValue(r);
+    }
+}
