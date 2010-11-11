0f525dd6fb83e455c6c734d6149442c952c8203d
Charles Care <c.p.care@gmail.com>
2010-11-11 12:50:04 +0000

    Getting list of spaces .

---
+++ src/main/java/ccare/domain/TableReference.java
@@ -0,0 +1,11 @@
+package ccare.domain;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: carecx
+ * Date: 11-Nov-2010
+ * Time: 08:44:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class TableReference {
+}
